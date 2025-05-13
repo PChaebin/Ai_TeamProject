766d1c7f772483690d235f218e5793de33196866
minwn3318 <minwn3318@naver.com>
2025-05-13 22:26:31 +0900

    create Item object
    투사체, 비밀번호, 아이템스포너 오브젝트 생성

---
+++ Ai_Project_Team_4/Assets/_Scripts/System/ItemSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject password;
+    public GameObject item;
+
+    public List<GameObject> passwordList;
+    public List<GameObject> itemList;
+
+    public int passwordNum = 4;
+    public int itemNum = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
