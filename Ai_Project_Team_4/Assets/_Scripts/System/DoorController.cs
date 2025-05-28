63e86c6ab4d365964c02122706e262052c08368d
yoods12 <45788545+yoods12@users.noreply.github.com>
2025-05-28 22:48:24 +0900

    map
    map

---
+++ Ai_Project_Team_4/Assets/_Scripts/System/DoorController.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DoorController : MonoBehaviour
+{
+    public List<int> passwordList;
+
+    private void Start()
+    {
+        //passwordList = ItemSpawner.GetPasswordList();
+    }
+}
